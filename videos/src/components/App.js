4e2efc72257d921ae0ef8b46310b5cef43bc3a15
Alex Larcheveque <larchevequealex@gmail.com>
2019-10-27 22:20:22 -0700

    Finished basic functionality of the Youtube. Loads video list from YouTube API, and allows user to play video.

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,11 +1,48 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../api/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  //We use the async and await keywords to wait for the response from Youtube
+  onTermSubmit = async term => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term
+      }
+    });
+
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
+    console.log(this.state.videos);
+  };
+
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="ten wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="six wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
